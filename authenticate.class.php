e0b2de96d1acca2b4da8c0cb50403a97ee82d778
Kausheel Kumar <kausheelkumar@gmail.com>
2012-07-18 21:42:58 +1000

    Create template of all functions necessary in the Auth class

---
+++ authenticate.class.php
@@ -0,0 +1,65 @@
+<?php
+
+	require('Database.class.php');
+	
+	class Authenticate {
+		
+		function __construct()
+		{
+			
+			
+				
+		}
+		
+		
+		function login($username, $password) 
+		{
+			
+			
+			
+			
+		}		
+		
+		function createUser($username, $password, $confirmpassword, $email)
+		{
+			
+			
+			
+		}
+		
+		function logout() 
+		{
+			
+			
+			
+		}
+		
+		function resetPassword($username)
+		{
+			
+			
+			
+		}
+		
+		
+		function isUserActive($active)
+		{
+						
+			
+			
+		}
+		
+		function changePassword($username, $password, $newpassword, $confirmnewpassword)
+		{
+			
+			
+			
+		}		
+				
+		
+		
+		
+		
+	}
+
+?>
