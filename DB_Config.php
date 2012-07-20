05a305f6afc299459a78ad7639d7af4d54bfa4f0
Kausheel Kumar <kausheelkumar@gmail.com>
2012-07-20 21:41:23 +1000

    Add parameters defining table structure

--- DB_Config.php
+++ DB_Config.php
@@ -1,8 +1,15 @@
 <?php
 	
+			//Set database login parameters
 			define('HOST', 'localhost');
 			define('DB_USERNAME', 'my_username');
 			define('DB_PASSWORD', 'my_password');
 			define('DATABASE_NAME', 'my_database');
+			
+			//Set table & column names containing the required information (usernames, email addresses, passwords)
+			define('TABLE_OF_USERS', 'my_table_name');
+			define('COLUMN_OF_USERS', 'column_name');
+			define('COLUMN_OF_EMAILS', 'column_name');
+			define('COLUMN_OF_PASSWORDS', 'column_name');
 			
 ?>
