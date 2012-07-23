34d4c31b19dd85cbfc96eeedc238fdad7f143710
Kausheel Kumar <kausheelkumar@gmail.com>
2012-07-23 10:59:44 +1000

    Add login() call

--- test.php
+++ test.php
@@ -2,13 +2,21 @@
     
     //The purpose of this code is to run tests on the Authenticate class
 	require('Authenticate.class.php');
-	
-	$object = new Authenticate();
-	
-	$object -> createUser($username, $password, $confirmpassword, $email);
-	$object -> login($username, $password);
+    
+	$object = new Authenticate();    
+    
+    //$object -> createUser($username, $password, $confirmpassword, $email);
+	if($object -> login('kausheel', 'a_password'))
+	{
+	    print 'You have logged in successfully!';
+    }
+    else
+    {
+        print 'Wrong password';
+    }
+        
 	$object -> changePassword($username, $password, $newpassword, $confirmnewpassword);
 	$object -> resetPassword($username, $email);
-	$object -> logout();
+	//$object -> logout();
     
 ?>
