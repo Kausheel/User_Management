579dc804c6e6939a52deea59ad3316f90809787b
Kausheel Kumar <kausheelkumar@gmail.com>
2012-07-23 10:33:34 +1000

    Remove email parameter. The email address will be used to login, $username will be the email address, so no need for a separate $email variable

--- DB_Config.php
+++ DB_Config.php
@@ -1,5 +1,5 @@
 <?php
-	
+	       
 			//Set database login parameters
 			define('HOST', 'localhost');
 			define('DB_USERNAME', 'my_username');
@@ -9,7 +9,7 @@
 			//Set table & column names containing the required information (usernames, email addresses, passwords)
 			define('TABLE_OF_USERS', 'my_table_name');
 			define('COLUMN_OF_USERS', 'column_name');
-			define('COLUMN_OF_EMAILS', 'column_name');
 			define('COLUMN_OF_PASSWORDS', 'column_name');
+  
 			
 ?>
