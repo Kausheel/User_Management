cbaef87a6c79ba0a2d0f3175542a55589adf8900
Kausheel Kumar <kausheelkumar@gmail.com>
2012-07-31 13:55:29 +1000

    Import the PHPMailer class into the validate_email() function. Create account_activated() function to check if the emailed link has been clicked

--- Authenticate.class.php
+++ Authenticate.class.php
@@ -1,8 +1,9 @@
 <?php
 
-	require('Encrypt.class.php');
+	include('Encrypt.class.php');
+    include('class.phpmailer.php');
 	
-	class Authenticate 
+    class Authenticate 
 	{
 	    //Define database login details
 		private $db_host = YOUR_HOSTNAME;
@@ -14,7 +15,8 @@
 		private $user_table = TABLE_OF_USERS;
 		private $email_col = COLUMN_OF_EMAILS;
 		private $password_col = COLUMN_OF_PASSWORDS;
-        private $validation_hash = COLUMN_OF_EMAIL_VALIDATION_HASHES;
+        private $email_validation_hash = COLUMN_OF_EMAIL_VALIDATION_HASHES;
+        private $activated = COLUMN_CONFIRMING_ACCOUNT_ACTIVATION;
 		
         private $mysqli;
         
@@ -30,18 +32,12 @@
 		    if($email && $password)
             {
                 //Fetch password from database
-                if($stmt = $this->mysqli->prepare("SELECT `$this->password_col` FROM `$this->user_table` WHERE `$this->email_col` = ?"))
-                {
-                    $stmt->bind_param('s', $email);
-                    $stmt->execute();
-                    $stmt->bind_result($hash);
-                    $stmt->fetch();
-                }
-                else
-                {
-                    return FALSE;
-                }
-                
+                $stmt = $this->mysqli->prepare("SELECT `$this->password_col` FROM `$this->user_table` WHERE `$this->email_col` = ?");
+                $stmt->bind_param('s', $email);
+                $stmt->execute();
+                $stmt->bind_result($hash);
+                $stmt->fetch();
+                                
                 //Check if the password hashes match
                 $encrypt = new Encrypt(12, FALSE);            
                 if($encrypt->check_password($password, $hash))
@@ -61,13 +57,13 @@
                     $password = $encrypt->hash_password($password);
                     
                     //Add user to database
-                    if($stmt = $this->mysqli->prepare("INSERT INTO `$this->user_table`(`$this->email_col`, `$this->password_col`) VALUES(?, ?)"))
-                    {    
-                        $stmt->bind_param('ss', $email, $password);
-                        $stmt->execute();
-                        $stmt->fetch();
-                    }
+                    $stmt = $this->mysqli->prepare("INSERT INTO `$this->user_table`(`$this->email_col`, `$this->password_col`) VALUES(?, ?)");   
+                    $stmt->bind_param('ss', $email, $password);
+                    $stmt->execute();
+                    $stmt->fetch();
                     
+                    validate_email($username);
+                                        
                     if(!$this->mysqli->error)
                     {
                         return TRUE;
@@ -78,25 +74,78 @@
 			
 		}
         
-        function validate_email()
+        function validate_email($username)
         {
             //Check if the $random_hash has been used before, and if yes, then generate another one until a unique hash is found 
-            if($stmt = $this->mysqli->prepare("SELECT `$this->validation_hash` FROM `$this->user_table` WHERE `$this->validation_hash` = ? LIMIT 1"))
+            $stmt = $this->mysqli->prepare("SELECT `$this->email_validation_hash` FROM `$this->user_table` WHERE `$this->email_validation_hash` = ? LIMIT 1");
+            do
             {
-                do
-                {
-                    $random_hash = md5(uniqid(rand(), true));    
-                    $stmt->bind_param('s', $random_hash);
-                    $stmt->execute();
-                    $stmt->bind_result($validation_hash);
-                    $stmt->fetch();
-                }
-                while($validation_hash == $random_hash);                                     
+                $random_hash = md5(uniqid(rand(), true));    
+                $stmt->bind_param('s', $random_hash);
+                $stmt->execute();
+                $stmt->bind_result($email_validation_hash);
+                $stmt->fetch();
+            }
+            while($email_validation_hash == $random_hash);                                     
+            
+            //If the hash was correctly generated, insert it into the database
+            if(strlen($random_hash) == 32)
+            {
+                $stmt = $this->mysqli->prepare("INSERT INTO `$this->user_table`(`$this->email_validation_hash`) VALUES(?) WHERE `$this->email_col` = ?");
+                $stmt->bind_param('ss', $random_hash, $username);
+                $stmt->execute();
+            }
+            
+            $mail = new PHPMailer();
+            
+            $mail->IsSMTP();                                        //Set mailer to use SMTP
+            $mail->Host = "smtp1.example.com;smtp2.example.com";    //Specify main and backup server
+            $mail->SMTPAuth = true;                                 //Turn on SMTP authentication
+            $mail->Username = "myusername";                         //SMTP username
+            $mail->Password = "secretpassword";                     //SMTP password
+            
+            $mail->From = "from@example.com";                       //Sender
+            $mail->FromName = "Mailer";
+            $mail->AddAddress("josh@example.net", "Josh Adams");    //Recipient    
+            $mail->AddReplyTo("info@example.com", "Information");   //Optional reply to address
+            $mail->WordWrap = 50;                                   //Set word wrap to X amount of characters
+            $mail->IsHTML(true);                                    //Set email format to HTML
+            
+            $mail->Subject = "Account registration";
+            $mail->Body    = "You have received this email because this address was used to register at our website. 
+                              If this was you, please click the link below:  
+                              http://localhost/Authentication/test.php?hash=$random_hash"; //The website URL for receiving the hash as a GET variable
+            $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+            
+            if(!$mail->Send())
+            {
+                return FALSE;
+            }
+            else
+            {
+                return TRUE;
             }
             
             
         }
 		
+        //Mark the account as activated
+        function account_activated($hash)
+        {
+            //Find the hash in the database, and mark the corresponding 'Activated" field to TRUE
+            $stmt = $this->mysqli->prepare("UPDATE `$this->user_table` SET `$this->activated` = 'TRUE' WHERE `$this->email_validation_hash` = ?");
+            $stmt->bind_param('s', $hash);
+            $stmt->execute();
+            
+            if($this->mysqli->error)
+            {
+                return FALSE;
+            }
+            else
+            {
+                return TRUE;
+            }   
+        }
 		function logout() 
 		{
 			
